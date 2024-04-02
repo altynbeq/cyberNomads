000af9753b842b93d7e45418041892ae7c1f8e13
altynbeq <starknbek@gmail.com>
2024-04-02 11:53:01 +0500

    fix v2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Добро пожаловать в
             <br />
-            <span className="text-[#915eff]"> Cyber Nomads V2</span>
+            <span className="text-[#915eff]"> Cyber Nomads V2F</span>
           </h1>
           <p className={`${styles.heroSubText} hidden  lg:block  mt-2 text-white-100`}>
           Ваш надежный партнер в автоматизации бизнес-процессов! <br/>
