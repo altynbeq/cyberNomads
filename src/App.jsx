c1a4ec75f187afdd9fd90e456317742bd4b80b84
altynbeq <starknbek@gmail.com>
2024-04-02 13:10:17 +0500

    version 3 fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
           <Hero />
         </div>
           <About />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
           <Experience />
           <Works />
         {/* <div className="relative z-0">   */}
