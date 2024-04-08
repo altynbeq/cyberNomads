853016cd1084824e255d1bb8bb3de0eb572bb904
altynbeq <starknbek@gmail.com>
2024-04-08 17:10:41 +0500

    menu btn fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full overflow-hidden flex items-center py-5 fixed top-0 z-10 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
         <Link 
           to="/" 
@@ -19,10 +19,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Cyber Nomads <span className='sm:block hidden'>| Digital Agency</span>
-          </p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Cyber Nomads <span className='sm:block hidden'>| Digital Agency</span></p>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
+        <ul className='list-none fixed z-10 hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => {
             return(
               <li key={link.id} 
@@ -36,13 +35,13 @@ const Navbar = () => {
             )
           })}
         </ul>
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img  src={menu} alt="menu" 
+        <div className='sm:hidden sm:fixed z-10 flex flex-1 justify-end items-center'>
+          <img  src={toggle ? close : menu} alt="menu" 
                 className='w-[28px] h-[28px] object-contain cursor-pointer'
                 onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+          <div className={`${!toggle ? 'hidden' : 'flex'} top-10 fixed overflow-hidden p-6 black-gradient  right-0 mx-4 my-2 min-w-[140px] z-15 rounded-xl`}>
+            <ul className='list-none flex  justify-end items-start flex-col gap-4'>
               {navLinks.map((link) => {
                 return(
                   <li key={link.id} 
