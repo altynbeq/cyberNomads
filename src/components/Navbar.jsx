db39ea03d16577db2b7d4346c023a7062d4a56b9
altynbeq <starknbek@gmail.com>
2024-04-11 23:03:35 +0500

    fixed fcking finally

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Altynbek <span className='sm:block hidden'>| Personal Portfolio</span>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Cyber Nomads <span className='sm:block hidden'>| Digital Agency</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
