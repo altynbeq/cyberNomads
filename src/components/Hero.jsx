06b92307a243cddf7bac174da94d685297705451
altynbeq <starknbek@gmail.com>
2024-04-16 12:14:22 +0500

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
 
   return (
     <section className='relative w-full h-screen mx-auto flex flex-col'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5 mb-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[90px] max-w-7x1 mx-auto flex flex-row items-start gap-5 mb-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
 
-        <div>
+        <div className='xs:top-2'>
           <h1 className={`${styles.heroHeadText} text-white overflow-hidden`}>Добро пожаловать в
             <br />
             <span className="text-[#915eff] z-50">Cyber Nomads</span>
